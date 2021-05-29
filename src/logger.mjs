eb4d17a1fb4d5e897ac9293dac062bdbe5bcbbee
Hendra Saputra <hendra.saputra@workday.com>
2021-05-29 12:29:51 +0100

    Configure winston as logger

---
+++ src/logger.mjs
@@ -0,0 +1,22 @@
+import winston from 'winston'
+const { createLogger, format, transports } = winston
+const { combine, timestamp, printf } = format
+
+const logFormat = printf(({ level, message, timestamp }) => {
+  return `${timestamp} [${level}]: ${message}`;
+})
+
+const logger = createLogger({
+  level: 'info',
+  format: combine(
+    timestamp(),
+    format.splat(),
+    format.colorize(),
+    logFormat
+  ),
+  transports: [
+    new transports.Console()
+  ]
+})
+
+export default logger
